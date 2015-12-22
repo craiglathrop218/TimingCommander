e227fa6335dbea0bc904aea494cd2e4f0bb0c963
craiglathrop218 <craiglathrop218@gmail.com>
2015-12-21 20:59:18 -0800

    Updated component data structures

--- public/app/devices/vc-five/data/vc-five-device.js
+++ public/app/devices/vc-five/data/vc-five-device.js
@@ -9,11 +9,17 @@ System.register([], function(exports_1) {
             VcFiveDevice = (function () {
                 function VcFiveDevice() {
                     // Settings and options
-                    this.configurationCount_options = { options: [1, 2, 3, 4],
+                    this.configurationCount_options = {
+                        value: 0,
+                        options: [1, 2, 3, 4],
                         limits: null };
-                    this.i2cAddress_options = { options: ['Primary 0xD4', 'Secondary 0xD9'],
+                    this.i2cAddress_options = {
+                        value: "",
+                        options: ['Primary 0xD4', 'Secondary 0xD9'],
                         limits: null };
-                    this.crystalLoadCap_options = { options: [6, 7, 8, 9, 10, 11, 12],
+                    this.crystalLoadCap_options = {
+                        value: 0,
+                        options: [6, 7, 8, 9, 10, 11, 12],
                         limits: null };
                 }
                 return VcFiveDevice;
--- public/app/devices/vc-five/data/vc-five-device.ts
+++ public/app/devices/vc-five/data/vc-five-device.ts
@@ -2,22 +2,23 @@
  * Created by IDT on 12/19/15.
  */
 
-
 export class VcFiveDevice {
     // Selection storage
-    public id:number;
-    public configurationCount:number;
-    public i2cAddress:string;
-    public crystalLoadCap:number;
+    public id:number; // Might not use this
 
     // Settings and options
-    public configurationCount_options = {options:[1,2,3,4],
+    public configurationCount_options = {
+        value:0,
+        options:[1,2,3,4],
         limits:null};
 
-    public i2cAddress_options = {options:['Primary 0xD4', 'Secondary 0xD9'],
+    public i2cAddress_options = {
+        value:"",
+        options:['Primary 0xD4', 'Secondary 0xD9'],
         limits:null};
 
-    public crystalLoadCap_options = {options:[6,7,8,9,10,11,12],
-        limits:null}
-
+    public crystalLoadCap_options = {
+        value:0,
+        options:[6,7,8,9,10,11,12],
+        limits:null};
 }
